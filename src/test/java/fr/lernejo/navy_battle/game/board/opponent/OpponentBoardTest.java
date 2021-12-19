e11bd7f1168c7c4dcfe249197ac34b71096fb2f2
Alexis Trupin <link9745@gmail.com>
2021-12-19 20:26:51 +0100

    adding printing in board tests

--- src/test/java/fr/lernejo/navy_battle/game/board/opponent/OpponentBoardTest.java
+++ src/test/java/fr/lernejo/navy_battle/game/board/opponent/OpponentBoardTest.java
@@ -9,8 +9,10 @@ public class OpponentBoardTest {
     @Test
     public void test() {
         final OpponentBoard board = new OpponentBoard();
+        System.out.println(board);
         Assert.assertNull(board.getCell(new CellCoordinates(5, 5)));
         board.setCell(new CellCoordinates(5, 5), OpponentBoard.Cell.HIT);
+        System.out.println(board);
         Assert.assertEquals(board.getCell(new CellCoordinates(5, 5)), OpponentBoard.Cell.HIT);
     }
 }
--- src/test/java/fr/lernejo/navy_battle/game/board/self/SelfBoardTest.java
+++ src/test/java/fr/lernejo/navy_battle/game/board/self/SelfBoardTest.java
@@ -9,15 +9,19 @@ public class SelfBoardTest {
     @Test
     public void test() {
         final SelfBoard board = new SelfBoard();
+        System.out.println(board);
         Assert.assertFalse(board.isValid());
         Assert.assertEquals(board.fire(new CellCoordinates(2, 3)), SelfBoard.Fire.MISS);
         final Boat boat1 = new Boat(2);
         board.addBoat(new CellCoordinates(6, 8), SelfBoard.Direction.VERTICAL, boat1);
+        System.out.println(board);
         Assert.assertEquals(board.fire(new CellCoordinates(7, 8)), SelfBoard.Fire.MISS);
         Assert.assertTrue(board.isValid());
         Assert.assertEquals(board.fire(new CellCoordinates(6, 8)), SelfBoard.Fire.HIT);
+        System.out.println(board);
         Assert.assertTrue(board.isValid());
         Assert.assertEquals(board.fire(new CellCoordinates(6, 9)), SelfBoard.Fire.SUNK);
+        System.out.println(board);
         Assert.assertFalse(board.isValid());
     }
 }
