9a837e4c64922c38d98392a7ed8ed2baf7c38f76
Alexis Trupin <link9745@gmail.com>
2021-12-20 22:55:46 +0100

    true test for api game start route

--- src/test/java/fr/lernejo/navy_battle/api/routes/ApiGameStartRouteTest.java
+++ src/test/java/fr/lernejo/navy_battle/api/routes/ApiGameStartRouteTest.java
@@ -1,5 +1,7 @@
 package fr.lernejo.navy_battle.api.routes;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import fr.lernejo.navy_battle.api.ApiResponse;
 import fr.lernejo.navy_battle.game.Game;
 import fr.lernejo.navy_battle.game.strategy.ComputerPlayer;
@@ -10,6 +12,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 public class ApiGameStartRouteTest {
 
@@ -27,6 +30,24 @@ public class ApiGameStartRouteTest {
         final ApiResponse response = this.route.handle("GET", new HashMap<>(), null);
         Assert.assertEquals(response.getStatus(), 404);
         Assert.assertEquals(response.getBody(), "Not Found: Method Not Allowed");
+    }
+
+    @Test
+    public void test() throws ExecutionException, InterruptedException {
+        final JsonObject body = new JsonObject();
+        final String id = UUID.randomUUID().toString();
+        final String url = "http://localhost:8080";
+        Assert.assertFalse(this.game.getOpponentId().isDone());
+        Assert.assertFalse(this.game.getOpponentUrl().isDone());
+        body.add("id", new JsonPrimitive(id));
+        body.add("url", new JsonPrimitive(url));
+        body.add("message", new JsonPrimitive("Test"));
+        final ApiResponse response = this.route.handle("POST", new HashMap<>(), body);
+        Assert.assertEquals(response.getStatus(), 202);
+        Assert.assertTrue(this.game.getOpponentId().isDone());
+        Assert.assertEquals(this.game.getOpponentId().get(), id);
+        Assert.assertTrue(this.game.getOpponentUrl().isDone());
+        Assert.assertEquals(this.game.getOpponentUrl().get(), url);
     }
 
     @After
